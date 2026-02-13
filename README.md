5b92fc8d5c8b75371f45349068efe021506a8ef3
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-13 17:06:47 -0500

    docs: add OpenAPI definition and README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Microservicio Persona
+
+Gestión de personas del sistema.
+
+## Requisitos
+
+- Java 25+
+- MySQL 8.0+
+- Gradle 9.3.0+
+
+## Levantar el servicio
+
+```bash
+./gradlew bootRun
+```
+
+El servicio estará disponible en `http://localhost:8081`
+
+**OpenAPI (Swagger):** `http://localhost:8081/swagger-ui.html`
+
+## Ver más
+
+Para información del proyecto completo, consulta el [repositorio raíz](https://github.com/Nekstoreo/programacion-reactiva).
--- src/main/java/com/onclass/persona/PersonaApplication.java
+++ src/main/java/com/onclass/persona/PersonaApplication.java
@@ -2,7 +2,16 @@ package com.onclass.persona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Microservicio Persona",
+        version = "0.0.1",
+        description = "API para gestión de personas del sistema"
+    )
+)
 @SpringBootApplication
 public class PersonaApplication {
 
